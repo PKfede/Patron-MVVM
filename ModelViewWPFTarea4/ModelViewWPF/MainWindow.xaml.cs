d3f16c935915c878701997b7340eda7e46781ac4
PKfede <47314404+PKfede@users.noreply.github.com>
2019-02-18 09:34:43 -0600

    Tareas 3, 4, 5 y 6

---
+++ ModelViewWPFTarea4/ModelViewWPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModelViewWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public static DataGrid datag;
+        int id;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void LoadBTN_Click(object sender, RoutedEventArgs e)
+        {
+            DataGrid1.ItemsSource = db.Customers.ToList();
+            datag = DataGrid1;
+            MessageBox.Show("Empresas clientes");
+        }
+    
+}
+}
